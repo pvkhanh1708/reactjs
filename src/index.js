b6d718f5c3a084266dfa6f790f63a2206fc000db
Kevin Nguyen <kevin.nguyen@hextrust.com>
2022-12-01 17:09:17 +0700

    Init

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react' // nạp thư viện react
+import ReactDOM from 'react-dom' // nạp thư viện react-dom
+
+// Tạo component App
+function App() {
+    return (
+        <div>
+            <h1>Hello world!</h1>
+            <h2>Hello world!</h2>
+            <h3>Hello world!</h3>
+            <h4>Hello world!</h4>
+            <h5>Hello world!</h5>
+            <h6>Hello world!</h6>
+
+        </div>
+    )
+}
+
+// Render component App vào #root element
+ReactDOM.render(<App />, document.getElementById('root'))
+
